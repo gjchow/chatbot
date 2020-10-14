1fa9b5e226980cb00f8ac439ece99f03e30d3f1b
gjchow <gjchow@users.noreply.github.com>
2020-10-14 19:56:19 -0400

    Minor changes and trained bot to be less rigid

--- information.py
+++ information.py
@@ -8,67 +8,67 @@ def course_info(course):
     if info == []:
         return ['Course not found']
     elif len(info) == 1:
-        return [info[0]]
-    return info
+        return info
+    return list(filter(None, info))
 
 
 def course_name(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     return [info[0]]
 
 
 def course_descrip(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     return [info[1]]
 
 
 def course_prereq(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     elif info[2] is None:
-        return 'Prerequisites:\nNone'
+        return ['Prerequisites: None']
     return [info[2]]
 
 
 def course_exclu(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     return [info[3]]
 
 
 def course_breadth(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     return [info[4]]
 
 
 def course_link(course):
     info = scrape(course)
     if info == []:
-        return 'Course not found'
+        return ['Course not found']
     elif len(info) == 1:
-        return info[0]
+        return info
     return [info[5]]
 
 
-def needed_in(course, code):
+def needed_in(course, code, show_details=True):
     valid = re.compile(r'^[a-zA-Z]{3}$')
     is_valid = re.match(valid, code)
     utsg = re.compile(r'^\w{3}[1-4]\d{2}$')
@@ -85,22 +85,25 @@ def needed_in(course, code):
     else:
         for i in range(400):
             to_check = code.upper() + str(i+100)
-            print(to_check)
-            start = time.perf_counter()
+            if show_details:
+                print(to_check)
+                start = time.perf_counter()
             prereq = course_prereq(to_check)
             if prereq != 'Course not found' and prereq != 'Not a valid course'\
                     and prereq != 'Prerequisites:\nNone':
                 if course in prereq[0]:
                     out.append(to_check)
                 check = True
-            end = time.perf_counter()
-            print(end-start)
-            times.append(end-start)
+            if show_details:
+                end = time.perf_counter()
+                print(end-start)
+                times.append(end-start)
+    if show_details:
+        print(sum(times))
+        print(sum(times) / 400)
     if not check:
         return [f'No courses starting with {code.upper()}']
     elif out == []:
         return [f'No courses starting with {code.upper()} need {course.upper()}']
     else:
-        print(sum(times))
-        print(sum(times)/400)
         return out
